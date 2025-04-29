afb0dfb5ef5a534d6a73520d46cbad5a9f043e38
Svetlana Bilous <sbilous@edu.lapinamk.fi>
2025-04-29 22:02:50 +0300

    port changed

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ def get_sentiment():
     return {'input_data': input_data, 'message': 'hello!'}
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000", debug=False)
+    app.run(host="0.0.0.0", port="8080", debug=False)
 
