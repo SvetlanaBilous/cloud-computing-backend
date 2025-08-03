2335701f041b9515b382fa896a98f68562209c9b
Svetlana Bilous <sbilous@edu.lapinamk.fi>
2025-08-03 15:51:21 +0300

    CHANGE

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     print('hello')
-    return "<p>Hello World from Svetlana UPDATE!</p>"
+    return "<p>Hello World from Svetlana CHANGE!</p>"
 
 @app.route('/sentiment', methods=['POST'])
 def get_sentiment():
